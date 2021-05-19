2b6e7b478802582472a8e080c0f3ccfa21f4d93d
Rahmouni <ferdaous.rahmouni@gmail.com>
2021-05-19 19:10:54 +0100

    secondCommit

--- mystateprofile/src/App.js
+++ mystateprofile/src/App.js
@@ -3,13 +3,69 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Component } from "react";
 import Nav from "./Components/Nav";
 import Profile from "./Components/Profile";
+import { Button } from "react-bootstrap";
 
 class App extends Component {
+  //rccp: react class component export
+  constructor(props) {
+    // rconst: react constructor
+    super(props);
+    this.state = {
+      Person: {
+        fullName: "Rahmouni Ferdaous",
+        bio: "I'am a FullStack JS Developer, Laravel Developer and Computer/Mobile Expert",
+        imgSrc: "/me.jpg",
+        profession: "IT Engineer",
+      },
+      shows: "true",
+      count: 0,
+    };
+    console.log("Profile is Initialised!");
+  }
+
+  OnToogle = () => {
+    this.setState({
+      shows: !this.state.shows,
+    });
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      if (!this.state.show) {
+        this.setState({ count: this.state.count + 1 });
+      } else {
+        this.setState({ count: 0 });
+      }
+    }, 1000);
+    console.log("Profile is Mounted!");
+  }
+  componentDidUpdate() {
+    console.log("Profile is Updated!");
+  }
   render() {
     return (
       <div className="App">
         <Nav />
-        <Profile />
+        {this.state.shows ? (
+          <Profile
+            fullName={this.state.Person.fullName}
+            bio={this.state.Person.bio}
+            imgSrc={this.state.Person.imgSrc}
+            profession={this.state.Person.profession}
+          />
+        ) : (
+          <span style={{ fontStyle: "italic", color: "purple" }}>
+            {this.state.count}
+          </span>
+        )}
+        <br />
+        <Button
+          onClick={this.OnToogle}
+          style={{ size: "lg", marginTop: "20px", marginBottom: "20px" }}
+          variant="info"
+        >
+          Toogle
+        </Button>
       </div>
     );
   }
--- mystateprofile/src/Components/Profile.js
+++ mystateprofile/src/Components/Profile.js
@@ -1,81 +1,22 @@
-import React, { Component } from "react";
-import { Container, Image, Button } from "react-bootstrap";
+import React from "react";
+import { Container, Image } from "react-bootstrap";
 
-class Profile extends Component {
-  // rccp: react class component
-  styles = {
-    fontStyle: "italic",
-    color: "teal",
-  };
-  constructor(props) {
-    // rconst: react constructor
-    super(props);
-    this.state = {
-      Person: {
-        fullName: "Rahmouni Ferdaous",
-        bio: "I'am a FullStack JS Developer, Laravel Developer and Computer/Mobile Expert",
-        imgSrc: "/me.jpg",
-        profession: "IT Engineer",
-      },
-      shows: "true",
-      count: 0,
-    };
-
-    console.log("Profile is Initialised!");
-  }
-
-  OnToogle = () => {
-    this.setState({
-      shows: !this.state.shows,
-    });
-  };
-
-  componentDidMount() {
-    setInterval(() => {
-      if (!this.state.show) {
-        this.setState({ count: this.state.count + 1 });
-      } else {
-        this.setState({ count: 0 });
-      }
-    }, 1000);
-    console.log("Profile is Mounted!");
-  }
-  componentDidUpdate() {
-    console.log("Profile is Updated!");
-  }
-
-  render() {
-    console.log("Profile is Rendred on the DOM!");
-    return (
-      <div>
-        {this.state.shows ? (
-          <Container style={this.styles}>
-            <Image
-              style={{ variant: "top", width: "250px", height: "280px" }}
-              src={this.state.Person.imgSrc}
-              roundedCircle
-            />
-            <h1>{this.state.Person.fullName}</h1>
-            <span>{this.state.Person.bio}</span>
-            <hr />
-            <h3>{this.state.Person.profession}</h3>
-          </Container>
-        ) : (
-          <span style={{ fontStyle: "italic", color: "purple" }}>
-            {this.state.count}
-          </span>
-        )}
-        <br />
-        <Button
-          onClick={this.OnToogle}
-          style={{ size: "lg", marginTop: "20px", marginBottom: "20px" }}
-          variant="info"
-        >
-          Toogle
-        </Button>
-      </div>
-    );
-  }
-}
+const Profile = ({ fullName, bio, imgSrc, profession }) => {
+  return (
+    <div>
+      <Container style={{ fontStyle: "italic", color: "teal" }}>
+        <Image
+          style={{ variant: "top", width: "250px", height: "280px" }}
+          src={imgSrc}
+          roundedCircle
+        />
+        <h1>{fullName}</h1>
+        <span>{bio}</span>
+        <hr />
+        <h3>{profession}</h3>
+      </Container>
+    </div>
+  );
+};
 
 export default Profile;
